df4df3b5cff5afc8d7e2f7ddc48e76da9c68c01f
João Guerreiro <joaolsguerreiro@gmail.com>
2017-02-09 09:15:57 +0000

    fixed the merge

--- src/org/academiadecodigo/bootcamp/escapeproject/Game/Room.java
+++ src/org/academiadecodigo/bootcamp/escapeproject/Game/Room.java
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-package org.academiadecodigo.bootcamp.escapeproject.Game;
-
-
-
-import org.academiadecodigo.bootcamp.escapeproject.Character.Sprite;
-
-/**
- * Created by codecadet on 05/02/17.
- */
-public class Room {
-
-
-
-   /* private Sprite sprite;
-    private int width;
-    private int heigth;
-    private boolean closed;
-
-
-    public Room(int width, int heigth){
-        this.heigth= heigth;
-        this.width = width;
-        sprite = new Sprite();
-
-
-    }
-
-
-
-public boolean isClosed(){
-    if(sprite.getPos);
-}*/
-
-
-}
-=======
 //package org.academiadecodigo.bootcamp.escapeproject.Game;
 //
 //
@@ -70,4 +33,3 @@ public boolean isClosed(){
 //
 //
 //}
->>>>>>> 28cf48601fdb73e51f412d4f581fe99fe4069e0e
